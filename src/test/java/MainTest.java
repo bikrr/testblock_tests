bf9ab54a3a387510e5f8a026c7e3ba21411e6473
Rustem Bikmukhametov <bikrr@mail.ru>
2019-05-14 13:46:34 +0500

    njtcn

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,32 +1,52 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainTest {
 
-    ChromeDriver driver; //объявляем заранее драйвер бразуера. Делать нужно вне методов
+    //***Windows***
+   // ChromeDriver driver; //объявляем заранее драйвер бразуера. Делать нужно вне методов
+
+    //***Ubuntu***
+    WebDriver driver;
 
     /*
     @BeforeMethod означает что будет выполянтся перед каждым методом, помеченным аннотацией Test
     alwaysRun означает что будет выпонятся всегда, даже если предыдущий тест упал
      */
     @BeforeMethod(alwaysRun = true)
-    public void beforeTest() {
-        System.setProperty("webdriver.chrome.driver","chromedriver.exe"); //обязательно указать путь до хромдрайвера
-        driver = new ChromeDriver();
+    public void beforeTest() throws MalformedURLException {
+
+        //***Windows***
+       // System.setProperty("webdriver.chrome.driver","chromedriver74.exe"); //обязательно указать путь до хромдрайвера
+       // driver = new ChromeDriver();
+
+        //***Ubuntu***
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver =  new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),options);
+
+
     }
 
     @Test
-    public void testOpenPageGoogle() {
-        driver.get("http://www.google.com");
-        Assert.assertTrue(driver.findElement(By.xpath("//input[@value='Поиск в Google']")).isDisplayed()); //Проверка на отображение элемента
+    public void testOpenPageGoogle() throws InterruptedException {
+        driver.get("http://localhost:8172");
+        Thread.sleep(2000);
+        Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='TestBlock test']")).isDisplayed()); //Проверка на отображение элемента
         driver.quit();
     }
 
     @Test
     public void testOpenPageYandex() throws InterruptedException {
-        driver.get("http://www.yandex.ru");
+        driver.get("http://localhost:8172");
         driver.quit();
     }
 
